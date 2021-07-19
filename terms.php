08cfd3f10fb837fab1575c9ab4e6e4b68d2cc265
ethicaldeveloper1 <87636366+ethicaldeveloper1@users.noreply.github.com>
2021-07-19 12:41:35 +0530

    Update terms.php

--- terms.php
+++ terms.php
@@ -23,7 +23,7 @@
               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="about.html">About Us</a>
+              <a class="nav-link" href="about.php">About Us</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="contact.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
